7187f51406c6f6d36bd25f941001c267835e1d54
Oscar Cambieri <oscar@cambieri.it>
2020-12-22 16:28:23 +0100

    Lesson 50-52: Alternative navigation bar; Recipe model; Recipe list static content.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Carbonara', 'Tipica pasta con pancetta ecc..', 'https://images.fidhouse.com/fidelitynews/wp-content/uploads/sites/6/2018/01/1517219140_f055c0757a72a3a48b267d0878cf5344e3a3f6b1-1517074283.png?w=580'),
+  ];
 
   constructor() { }
 
