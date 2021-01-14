2a67b9cc0e6b64b096ba20df051d9b13c23d6663
Oscar Cambieri <oscar@cambieri.it>
2021-01-14 09:01:15 +0100

    Lesson 55-59: ShoppingList; Ingredient model; ShoppingEdit.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Pancetta', 50),
+    new Ingredient('Pasta', 750)
+  ];
 
   constructor() { }
 
