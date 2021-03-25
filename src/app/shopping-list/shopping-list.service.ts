39fc557f11cbfdbebf7123d19d0eb5715abde6c3
Oscar Cambieri <oscar@cambieri.it>
2021-03-25 10:23:28 +0100

    Lesson 120-122: ShoppingListService up and running

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,22 @@
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+
 export class ShoppingListService {
+
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+  private ingredients: Ingredient[] = [
+    new Ingredient('Pancetta', 50),
+    new Ingredient('Pasta', 750)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 
 }
