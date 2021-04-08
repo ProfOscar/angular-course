3505d4e50fa92983f6456da50f607124d75b678f
Oscar Cambieri <oscar@cambieri.it>
2021-04-08 08:47:37 +0200

    Lessons 280 and 282: set up a DataStorageService to load the recipes (to be completed...)

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
+
+@Injectable()
+export class DataStorageService {
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) { }
+
+  fetchRecipes() {
+    this.http.get("http://localhost:3000/recipes/") // to be completed...
+  }
+
+}
