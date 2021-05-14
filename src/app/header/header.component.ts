953348af808aeb4782cacfbbad455dd9dcb17e03
Prof. Cambieri Oscar <oscar.cambieri@vallauri.edu>
2021-05-14 12:11:54 +0200

    Lesson 162 and 163: child routes for recipes now ok

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,6 +11,10 @@ export class HeaderComponent {
 
   constructor(private dataStorageService: DataStorageService) { }
 
+  ngOnInit() {
+    this.onFetchData();
+  }
+
   onFetchData() {
     this.dataStorageService.fetchRecipes();
   }
