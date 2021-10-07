80fa6cb51766f3c591ffd42fe8cb82042abc0240
Oscar Cambieri <oscar.cambieri@vallauri.net>
2021-10-07 10:50:13 +0200

    Lesson 1 - Step 3: ngStyle directive applied to student component

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -7,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+  studentList = [
+    { name: 'Ettore Esposito', hobby: 'Karate', gender: 'M' },
+    { name: 'Luca Pellissero', hobby: 'Paddle', gender: 'M' },
+    { name: 'Bianca Teleman', hobby: 'Volley', gender: 'F' },
+    { name: 'Paolo Giordano', hobby: 'Volley', gender: 'M' },
+    { name: 'Giada Valinotto', hobby: 'Paddle', gender: 'F' },
+    { name: 'Marta Baudracco', hobby: 'Volley', gender: 'F' },
+    { name: 'Michele Ghisolfi', hobby: 'Paddle', gender: 'M' },
+    { name: 'Martina Velardi', hobby: 'Karate', gender: 'F' },
+    { name: 'Michele Ghisolfi', hobby: 'Paddle', gender: 'M' },
+    { name: 'Paolo Racca', hobby: 'Paddle', gender: 'M' },
+    { name: 'Ivan Anjelovski', hobby: 'Karate', gender: 'M' },
+    { name: 'Gabriele Leone', hobby: 'Volley', gender: 'M' },
+  ]
+
+  student: any;
+
+  constructor() {
+    let num = Math.floor(Math.random() * this.studentList.length);
+    this.student = this.studentList[num];
+  }
 
   ngOnInit(): void {
   }
