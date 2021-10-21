9617ebfbf7a7413886b03a7a2b6b0975b2607146
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2021-10-21 11:41:04 +0200

    ngModel and ViewChild to add new students to the list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,20 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild("txtName") txtName!: ElementRef;
+  // = new ElementRef(null);
+
   title = '5 INF B 2021/22';
 
-  studentList = [
+  studentRepository = [
     { name: 'Ettore Esposito', hobby: 'Karate', gender: 'M', isPro: false },
     { name: 'Luca Pellissero', hobby: 'Paddle', gender: 'M', isPro: false },
     { name: 'Bianca Teleman', hobby: 'Volley', gender: 'F', isPro: false },
     { name: 'Paolo Giordano', hobby: 'Volley', gender: 'M', isPro: false },
     { name: 'Giada Valinotto', hobby: 'Paddle', gender: 'F', isPro: false },
     { name: 'Marta Baudracco', hobby: 'Volley', gender: 'F', isPro: false },
-    { name: 'Michele Ghisolfi', hobby: 'Paddle', gender: 'M', isPro: false },
     { name: 'Martina Velardi', hobby: 'Karate', gender: 'F', isPro: false },
     { name: 'Michele Ghisolfi', hobby: 'Paddle', gender: 'M', isPro: false },
     { name: 'Paolo Racca', hobby: 'Paddle', gender: 'M', isPro: false },
     { name: 'Ivan Anjelovski', hobby: 'Karate', gender: 'M', isPro: false },
     { name: 'Gabriele Leone', hobby: 'Volley', gender: 'M', isPro: false },
   ]
+
+  hobbies = ['Karate', 'Paddle', 'Volley', 'Tennis', 'Soccer'];
+
+  studentList: any[] = [];
+
+  studentName: string = "";
+  studentGender: string = "F";
+  studentHobbie: string = "";
+
+  constructor() {
+    for (let index = 0; index < 4; index++) {
+      let num = Math.floor(Math.random() * this.studentRepository.length);
+      let tempStudent = this.studentRepository[num];
+      this.studentList.push(tempStudent);
+      this.studentRepository.splice(num, 1);
+    }
+  }
+
+  onAddStudent() {
+    let newStudent = {
+      name: this.studentName,
+      hobby: this.studentHobbie,
+      gender: this.studentGender,
+      isPro: false
+    };
+    this.studentList.push(newStudent);
+    this.studentName = "";
+    this.txtName.nativeElement.focus();
+  }
 }
