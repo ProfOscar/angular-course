450a2b4b8a5af30cec9bdd55932dc6769bfc7da8
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2022-01-13 10:23:20 +0100

    Patch and post to save the shopping list data and revert to json-server

--- src/app/shared/shopping-list.service.ts
+++ src/app/shared/shopping-list.service.ts
@@ -3,23 +3,22 @@ import { DataStorageService } from './data-storage.service';
 import { IngredientModel } from '../models/ingredient.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShoppingListService {
-
   ingredients: IngredientModel[] = [];
 
-  constructor(private dataStorageService: DataStorageService) { }
+  constructor(private dataStorageService: DataStorageService) {}
 
   getIngredients() {
     this.dataStorageService.sendGetRequest('shopping-list').subscribe(
-      data => {
+      (data) => {
         this.ingredients = data as IngredientModel[];
       },
-      error => {
+      (error) => {
         console.error(error);
       }
-    )
+    );
   }
 
   addIngredient(ingredient: IngredientModel) {
@@ -28,13 +27,43 @@ export class ShoppingListService {
       if (item.name.toLowerCase() == ingredient.name.toLowerCase()) {
         ingredientFound = true;
         item.amount += ingredient.amount;
+        this.patchIngredient({ amount: item.amount }, item.id);
         break;
       }
     }
-    if (!ingredientFound) this.ingredients.push(ingredient);
+    if (!ingredientFound) {
+      this.ingredients.push(ingredient);
+      this.postIngredient(ingredient);
+    }
   }
 
   addIngredients(ingredients: IngredientModel[]) {
     this.ingredients.push(...ingredients);
   }
+
+  postIngredient = (ingredient: IngredientModel) => {
+    this.dataStorageService
+      .sendPostRequest('shopping-list', ingredient)
+      .subscribe(
+        (succ) => {
+          console.log(succ);
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
+  };
+
+  patchIngredient = (data: any, id: number) => {
+    this.dataStorageService
+      .sendPatchtRequest('shopping-list/' + id, data)
+      .subscribe(
+        (succ) => {
+          console.log(succ);
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
+  };
 }
