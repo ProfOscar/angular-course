a47ec4759097dc12e4e09da24d81459449ad6b72
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2022-02-17 11:40:51 +0100

    Child routes on recipes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 const appRoutes: Routes = [
   {
@@ -13,6 +15,10 @@ const appRoutes: Routes = [
   {
     path: 'recipes', // senza slash !
     component: RecipesComponent,
+    children: [
+      { path: '', component: RecipeStartComponent },
+      { path: ':id', component: RecipeDetailComponent }
+    ]
   },
   {
     path: 'shopping-list', // senza slash !
